0859cc1b0b7b3d7b74493ce843f25b4cd8b74370
Hridaya Agrawal <hridayahoney@gmail.com>
2021-04-26 21:18:03 +0530

    Create main.py

---
+++ Day 1: Print, Len, Input, Variables/(Coding Exercise) Variables/Solution/main.py
@@ -0,0 +1,16 @@
+num_1 = input("Number 1: ")
+num_2 = input("Number 2: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+no1Reversed = num_2
+no2Reversed = num_1
+num_1 = no1Reversed
+num_2 = no2Reversed
+
+#Write your code above this line 👆
+
+# 🚨 Don't change the code below 👇
+print(f"Number 1: {num_1}")
+print(f"Number 2 {num_2}")
