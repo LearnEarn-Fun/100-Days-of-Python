f92b06760f7813c794e44402a3f513ae66de9077
Hridaya Agrawal <hridayahoney@gmail.com>
2021-04-29 16:00:07 +0530

    Update main.py

--- Day 5: Python Loops/Day 5 Project: Password Generator/main.py
+++ Day 5: Python Loops/Day 5 Project: Password Generator/main.py
@@ -43,6 +43,6 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '~', '^', '@', '{', '}',
 
 print(welcome)
 print(logo)
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+no_letters= int(input("How many letters would you like in your password?\n")) 
+no_symbols = int(input(f"How many symbols would you like?\n"))
+no_numbers = int(input(f"How many numbers would you like?\n"))
