ba5c0807df25fd9e6cca20d04ffab1fc79adfda0
Hridaya Agrawal <hridayahoney@gmail.com>
2021-04-29 16:01:13 +0530

    Update main.py

--- Day 5: Python Loops/Day 5 Project: Password Generator/Solution/main.py
+++ Day 5: Python Loops/Day 5 Project: Password Generator/Solution/main.py
@@ -43,19 +43,19 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '~', '^', '@', '{', '}',
 
 print(welcome)
 print(logo)
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+no_letters= int(input("How many letters would you like in your password?\n")) 
+no_symbols = int(input(f"How many symbols would you like?\n"))
+no_numbers = int(input(f"How many numbers would you like?\n"))
 
 password_list = []
 
-for char in range(1, nr_letters + 1):
+for char in range(1, no_letters + 1):
   password_list += random.choice(letters)
 
-for char in range(1, nr_symbols + 1):
+for char in range(1, no_symbols + 1):
   password_list += random.choice(symbols)
 
-for char in range(1, nr_numbers + 1):
+for char in range(1, no_numbers + 1):
   password_list += random.choice(numbers)
 
 
