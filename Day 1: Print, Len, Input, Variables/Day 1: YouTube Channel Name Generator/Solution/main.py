92f86a22145d26323857daea9265ca189d3f77fe
Hridaya Agrawal <hridayahoney@gmail.com>
2021-06-22 18:09:31 +0530

    Update main.py

--- Day 1: Print, Len, Input, Variables/Day 1: YouTube Channel Name Generator/Solution/main.py
+++ Day 1: Print, Len, Input, Variables/Day 1: YouTube Channel Name Generator/Solution/main.py
@@ -45,10 +45,10 @@ print(title)
 channel_topic = input("Which topic wiil your YouTube Videos Be? ") 
 
 channel_name = "Tech With " + channel_topic
-channel_name1 = f"{channel_topic} Master"
+channel_name1 = f"Master {channel_topic}!"
 channel_name2 = channel_topic + " Guru"
 channel_name3 = f"Learn {channel_topic}!"
 
-print(f"Here are some suggestions for your YouTube Channel:  {channel_name}, {channel_name1}, {channel_name2}, {channel_name3}")
+print(f"Here are some suggestions for your YouTube Channel:  \n{channel_name}, {channel_name1}, {channel_name2}, {channel_name3}")
 print("Thank You for using your YouTube Channel Name Generator!")
 
